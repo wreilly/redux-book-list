59d89e57e3f8bf77489d1646d11bca6ac12502f4
William Reilly <wreilly2001@gmail.com>
2016-10-11 13:20:06 -0400

    Initial commit, app /src code from Instructor repo

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,20 @@
+export default function() {
+    return [
+	    {
+		title: 'JavaScript: Bueno Partitos',
+		    pages: 101
+	    },
+	    {
+		title: 'Potter, Jarry',
+		    pages: 399
+	    },
+	    {
+		title: 'Dark Tour',
+		    pages: 4
+	    },
+	    {
+		title: 'Something about Ruby',
+		    pages: 1
+	    },
+	    ];
+}
